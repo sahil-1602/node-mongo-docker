084dea531f397a97a2971ecfaa4e86abaad66c5a
sahil-1602 <sahildarji16@gmail.com>
2023-07-26 09:44:53 +0530

    Mongo Node - Docker project : Initial commit

---
+++ index.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+const express = require("express");
+
+const app = express();
+
+const DATABASE = 'mongodb://mymongo:27017/dockertest';
+
+mongoose
+  .connect(DATABASE)
+  .then(() => {
+    console.log('DB Connected!');
+  })
+  .catch(() => {
+    console.log('Error in DB connection!');
+  });
+
+
+app.get("/", (req, res) => {
+    res.json({
+        message: "You are visiting ROOT route"
+    });
+});
+
+app.listen(8000, () => {
+    console.log("8000 is ready to serve...");
+});
